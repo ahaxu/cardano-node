88a153a9de02e28863d3175d13f9d7cba361a04a
Karl Knutsson <karl.knutsson@iohk.io>
2021-03-31 12:20:25 +0200

    Update ouroboros-network

--- cardano-node/src/Cardano/Tracing/OrphanInstances/Network.hs
+++ cardano-node/src/Cardano/Tracing/OrphanInstances/Network.hs
@@ -376,6 +376,7 @@ instance HasSeverityAnnotation (TracePeerSelection addr) where
       TraceDemoteAsynchronous    {} -> Info
       TraceGovernorWakeup        {} -> Info
       TraceChurnWait             {} -> Info
+      TraceXXX                   {} -> Info
 
 instance HasPrivacyAnnotation (DebugPeerSelection addr conn)
 instance HasSeverityAnnotation (DebugPeerSelection addr conn) where
